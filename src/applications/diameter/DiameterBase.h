95e34a80eb19042a4e06fdbc88b37e710973c97a
calincerchez <cherokeu@yahoo.com>
2012-07-03 09:58:54 +0300

    test

--- src/applications/diameter/DiameterBase.h
+++ src/applications/diameter/DiameterBase.h
@@ -26,6 +26,50 @@
 #include "DiameterPeerTable.h"
 #include "RoutingTableAccess.h"
 
+/*
+ * Implements the SCTP protocol. This section describes the internal
+ * architecture of the SCTP model.
+ *
+ *   You may want to check the SCTPSocket
+ * class which makes it easier to use SCTP from applications.
+ *
+ * The SCTP protocol implementation is composed of several classes (discussion
+ * follows below):
+ *   - SCTP: the module class
+ *   - SCTPAssociation: manages a connection
+ *   - SCTPSendQueue, SCTPReceiveQueue: abstract base classes for various types
+ *      of send and receive queues
+ *   - SCTPAlgorithm: abstract base class for SCTP algorithms
+ *
+ * SCTP subclassed from cSimpleModule. It manages socketpair-to-connection
+ * mapping, and dispatches segments and user commands to the appropriate
+ * SCTPAssociation object.
+ *
+ * SCTPAssociation manages the connection, with the help of other objects.
+ * SCTPAssociation itself implements the basic SCTP "machinery": takes care
+ * of the state machine, stores the state variables (TCB), sends/receives
+ *   etc.
+ *
+ * SCTPAssociation internally relies on 3 objects. The first two are subclassed
+ * from SCTPSendQueue and SCTPReceiveQueue. They manage the actual data stream,
+ * so SCTPAssociation itself only works with sequence number variables.
+ * This makes it possible to easily accomodate need for various types of
+ * simulated data transfer: real byte stream, "virtual" bytes (byte counts
+ * only), and sequence of cMessage objects (where every message object is
+ * mapped to a SCTP sequence number range).
+ *
+ * Currently implemented send queue and receive queue classes are
+ * SCTPVirtualDataSendQueue and SCTPVirtualDataRcvQueue which implement
+ * queues with "virtual" bytes (byte counts only).
+ *
+ * The third object is subclassed from SCTPAlgorithm. Control over
+ * retransmissions, congestion control and ACK sending are "outsourced"
+ * from SCTPAssociation into SCTPAlgorithm: delayed acks, slow start, fast rexmit,
+ * etc. are all implemented in SCTPAlgorithm subclasses.
+ *
+ * The concrete SCTPAlgorithm class to use can be chosen per connection (in OPEN)
+ * or in a module parameter.
+ */
 class DiameterBase : public cSimpleModule {
 public:
 	std::string fqdn;
@@ -56,6 +100,9 @@ public:
     void initialize(int stage);
     void handleMessage(cMessage *msg);
 
+    /*
+     *
+     */
     virtual DiameterMessage *processMessage(DiameterMessage *msg) { return NULL; }
 
     int getOutboundStreams() { return outboundStreams; }
