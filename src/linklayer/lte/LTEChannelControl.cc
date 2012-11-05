d9126a596db58acd34033acfb2f4547bdbde9d40
calincerchez <cherokeu@yahoo.com>
2012-11-05 18:17:16 +0200

    adding channel control

---
+++ src/linklayer/lte/LTEChannelControl.cc
@@ -0,0 +1,84 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "LTEChannelControl.h"
+
+Define_Module(LTEChannelControl);
+
+LTEChannelControl::LTEChannelControl() {
+
+}
+
+LTEChannelControl::~LTEChannelControl() {
+
+}
+
+LTEChannelControl::RadioRef LTEChannelControl::registerRadio(cModule *radio, cGate *radioInGate)
+{
+    Enter_Method_Silent();
+
+    RadioRef radioRef = lookupRadio(radio);
+
+    if (radioRef)
+        throw cRuntimeError("Radio %s already registered", radio->getFullPath().c_str());
+
+    if (!radioInGate)
+        radioInGate = radio->gate("radioIn");
+
+    RadioEntry re;
+    re.radioModule = radio;
+    re.radioInGate = radioInGate->getPathStartGate();
+    re.isNeighborListValid = false;
+    //re.channel = 0;  // for now
+    re.isActive = true;
+    radios.push_back(re);
+    return &radios.back(); // last element
+}
+
+void LTEChannelControl::unregisterRadio(RadioRef r)
+{
+    Enter_Method_Silent();
+    for (RadioList::iterator it = radios.begin(); it != radios.end(); it++)
+    {
+        if (it->radioModule == r->radioModule)
+        {
+            RadioRef radioToRemove = &*it;
+            // erase radio from all registered radios' neighbor list
+            for (RadioList::iterator i2 = radios.begin(); i2 != radios.end(); ++i2)
+            {
+                RadioRef otherRadio = &*i2;
+                otherRadio->neighbors.erase(radioToRemove);
+                otherRadio->isNeighborListValid = false;
+                radioToRemove->isNeighborListValid = false;
+            }
+
+            // erase radio from registered radios
+            radios.erase(it);
+            return;
+        }
+    }
+
+    error("unregisterRadio failed: no such radio");
+}
+
+LTEChannelControl::RadioRef LTEChannelControl::lookupRadio(cModule *radio)
+{
+    Enter_Method_Silent();
+    for (RadioList::iterator it = radios.begin(); it != radios.end(); it++)
+        if (it->radioModule == radio)
+            return &(*it);
+    return 0;
+}
+
---
+++ src/linklayer/lte/LTEChannelControl.h
@@ -0,0 +1,84 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef LTECHANNELCONTROL_H_
+#define LTECHANNELCONTROL_H_
+
+#include "IChannelControl.h"
+
+struct IChannelControl::RadioEntry {
+    cModule *radioModule;  // the module that registered this radio interface
+    cGate *radioInGate;  // gate on host module used to receive airframes
+    std::vector<int> channels;
+    Coord pos; // cached radio position
+
+    struct Compare {
+        bool operator() (const RadioRef &lhs, const RadioRef &rhs) const {
+            ASSERT(lhs && rhs);
+            return lhs->radioModule->getId() < rhs->radioModule->getId();
+        }
+    };
+    // we cache neighbors set in an std::vector, because std::set iteration is slow;
+    // std::vector is created and updated on demand
+    std::set<RadioRef, Compare> neighbors; // cached neighbor list
+    std::vector<RadioRef> neighborList;
+    bool isNeighborListValid;
+    bool isActive;
+};
+
+class LTEChannelControl : public cSimpleModule, public IChannelControl {
+
+protected:
+    typedef std::list<RadioEntry> RadioList;
+    typedef std::vector<RadioRef> RadioRefVector;
+
+    RadioList radios;
+
+    /** the number of controlled channels */
+    int numChannels;
+
+    /** Returns the "handle" of a previously registered radio. The pointer to the registering (radio) module must be provided */
+    virtual RadioRef lookupRadio(cModule *radioModule);
+public:
+    LTEChannelControl();
+    virtual ~LTEChannelControl();
+
+    /** Registers the given radio. If radioInGate==NULL, the "radioIn" gate is assumed */
+    virtual RadioRef registerRadio(cModule *radioModule, cGate *radioInGate = NULL);
+
+    /** Unregisters the given radio */
+    virtual void unregisterRadio(RadioRef r);
+
+    /** Returns the host module that contains the given radio */
+    virtual cModule *getRadioModule(RadioRef r) const { return r->radioModule; }
+
+    /** Returns the input gate of the host for receiving AirFrames */
+    virtual cGate *getRadioGate(RadioRef r) const { return r->radioInGate; }
+
+    /** Returns the channel the given radio listens on - dummy */
+    virtual int getRadioChannel(RadioRef r) const { return -1; }
+
+    /** To be called when the host moved; updates proximity info */
+    virtual void setRadioPosition(RadioRef r, const Coord& pos);
+
+    /** Called when host switches channel - dummy */
+    virtual void setRadioChannel(RadioRef r, int channel) {}
+
+    /** Returns the number of radio channels (frequencies) simulated */
+    virtual int getNumChannels() { return numChannels; }
+
+};
+
+#endif /* LTECHANNELCONTROL_H_ */
